9c0827da4157405e265d62f363a1685abdf92e3c
isreal <ogbuisreal22@gmail.com>
2023-09-30 22:59:50 +0100

    bank management application

---
+++ src/main/java/com/example/fingryd/FingrydApplication.java
@@ -0,0 +1,13 @@
+package com.example.fingryd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FingrydApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FingrydApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fingryd/FingrydApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fingryd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FingrydApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
